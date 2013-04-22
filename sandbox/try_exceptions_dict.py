74f0c38a5266ceda1779852f8c751cc0a8d67945
glphvgacs <darwinskernel@gmail.com>
2013-04-22 12:48:01 -0400

    deleted:    canned/credit 	modified:   try_exceptions_dict.py

--- sandbox/try_exceptions_dict.py
+++ sandbox/try_exceptions_dict.py
@@ -4,58 +4,67 @@ import os
 import sys
 import commands
 
-from sets import Set
-from sets import 
-
 #what time is it
 #what is the value of X - X is a string like (2+2)/1.5, report it's value
  
-cmd_classes  = {  'open':read_file
-		, 'execute':nix_cmd
-		, 'credits':info_cmd
-		, 'help':info_cmd
-		, 'dic':my_cmd
-		, 'geo':my_cmd
-		, 'log':my_cmd
+
+legal_nix_cmds  = {'uptime':1, 'ls':1, 'ping':1}
+py_what_cmd = {'time':1, 'is the value of':1}
+
+me_info  = { 'name':'foozy', 'family':'boozy' }
+
+#all_cmd_len = len(all_cmd)
+
+class cmds:
+	def __init__(self, msg):
+		self.cmd = msg[0]
+		self.dcv = msg[1]
+class py_cmds:
+	def ret(cmd):
+		return None
+class nix(cmds):
+	def ret(self, cmd):
+		return commands.getoutput(cmd)
+
+info_cmds_path_prefix = './canned/'
+class info_cmds:
+	def ret(self, info):
+		f =  info_cmds_path_prefix + info[0]
+		for line in open(f, 'r'):
+			ret += line
+		return ret
+class my_cmds:
+	def ret(my):
+		return None
+class wh_q:
+	def ret(whq):
+		return None
+cmd_classes  = {  'open':py_cmds
+		, 'execute':nix
+		, 'credits':info_cmds
+		, 'help':info_cmds
+		, 'dic':my_cmds
+		, 'geo':my_cmds
+		, 'log':my_cmds
 		, 'what':wh_q
 		, 'how':wh_q
 		}
-
-legal_nix_cmd  = {'uptime':1, 'ls':1, 'ping':1}
-py_what_cmd = ['time':1, 'is the value of':1]
-
-me_inf  = [ 'name':foozy, 'family':boozy ]
-
-all_cmd_len = len(all_cmd)
-
-class nix_cmds:
-	def ret(cmd):
-		return commands.getoutput("cmd")
-
-info_cmd_path_prefix = './canned/'
-class info_cmd:
-	def ret(info):
-		f =  info_cmd_path_prefix + info
-		for line in open(info.f, 'r'):
-			info.ret += line
-def classify(cmd):
-	if cmd in cmd_classes:
-		return cmd_classes[cmd]
-
+def classify(msg):
+	if msg[0] in cmd_classes:
+		return cmd_classes[msg[0]]
 def respond(message, sender=""):
-	message = message.strip()
-	#print "Got message",message
+	message   = message.strip()
 	words     = message.split()
-	cmd       = words[0]
-	cmd_class = classify(cmd)
-	print cmd_class.ret(cmd)
+	cmd_class = classify(words)
+	cmd_class_ins = cmd_class(words)
+	print cmd_class_ins.ret(cmd_class_ins.dcv)
+	#print cmd_class_ins.ret(words)
 
-
-respond('open    foo',    'me')
+#respond('open    foo',    'me')
 
 respond('execute uptime', 'me')
 respond('execute ls',     'me')
-respond('execute ping 127.0.0.1', 'me') # TODO: ask if they're gonna be throwing ipv6
+#respond('execute ping 127.0.0.1', 'me') # TODO: ask if they're gonna be throwing ipv6
 
-respond('credit', 'me')
-respond('help'  , 'me')
+#respond('credits', 'me')
+#respond('help'  , 'me')
