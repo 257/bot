673910bd1b19b34202883efff157788602441e2e
glphvgacs <darwinskernel@gmail.com>
2013-04-24 15:58:11 -0400

    get location _and_ coordination 	modified:   weather.py

--- sandbox/weather.py
+++ sandbox/weather.py
@@ -9,7 +9,25 @@ from xml.etree import ElementTree as ET
 #print ET.fromstring(yourxml).find('//Hostip/{http://opengis.net.gml}name').text
 root = ET.fromstring(yourxml)
 #print root.findall("Hostip")
-for child in root:
-	for cchild in child:
-		for yachild in cchild:
-			print yachild.tag, yachild.attrib
+#for child in root:
+#	print child.tag, child.attrib
+#	for cchild in child:
+#		print cchild.tag, cchild.attrib
+#		for yachild in cchild:
+#			print yachild.tag, yachild.attrib
+#			for yac in yachild:
+#				print yac.tag, yac.attrib
+
+#print root[1].tag
+#for neighbor in root.iter():
+#	print neighbor.text
+for neighbor in root.iter('countryName'):
+	country = neighbor.text
+for child in root.iter('ip'):
+	ip = child.text
+for child in root[3].iter('{http://www.opengis.net/gml}name'):
+	city = child.text
+for child in root.iter('{http://www.opengis.net/gml}coordinates'):
+	print child.text
+
+#print ip, city, country
